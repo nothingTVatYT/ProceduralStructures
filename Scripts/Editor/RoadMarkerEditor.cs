14edcdc4e603c0d938d70785bfc9077d76d1ed58
nothingTVatYT <rjakob13@googlemail.com>
2021-08-26 21:32:36 +0200

    fix multi-edit

--- Scripts/Editor/RoadMarkerEditor.cs
+++ Scripts/Editor/RoadMarkerEditor.cs
@@ -7,11 +7,21 @@ public class RoadMarkerEditor : Editor
     public override void OnInspectorGUI() {
         DrawDefaultInspector();
         if (GUILayout.Button("Drop to terrain")) {
-            RoadMarker marker = (RoadMarker)target;
-            Undo.RecordObject(marker.gameObject.transform, "Drop to terrain");
-            Vector3 pos = marker.gameObject.transform.position;
-            pos.y = Terrain.activeTerrain.SampleHeight(pos) + 1;
-            marker.gameObject.transform.position = pos;
+            if (targets != null) {
+                foreach (Object obj in targets) {
+                    DropToTerrain(obj as RoadMarker);
+                }
+            } else {
+                RoadMarker marker = (RoadMarker)target;
+                DropToTerrain(marker);
+            }
         }
+    }
+
+    public void DropToTerrain(RoadMarker marker) {
+        Undo.RecordObject(marker.gameObject.transform, "Drop to terrain");
+        Vector3 pos = marker.gameObject.transform.position;
+        pos.y = Terrain.activeTerrain.SampleHeight(pos) + 1;
+        marker.gameObject.transform.position = pos;
     }
 }
